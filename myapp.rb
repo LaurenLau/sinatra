4d4a4037623f856017c53be3bf20747ce07c0b57
Laurenlau <harris_lau@hotmail.co.uk>
2019-08-06 14:08:40 +0100

    Introducing params

--- myapp.rb
+++ myapp.rb
@@ -9,7 +9,13 @@ get '/secret' do
   'Hierhelpds[njcjfdsew;m'
 end
 
-get '/cat' do
-  @name = ['Amigo', 'Oscar', 'Viking'].sample
+get '/random-cat' do
+  @name =['Amigo', 'Oscar', 'Viking'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
